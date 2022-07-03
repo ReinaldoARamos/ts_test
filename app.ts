d7640dcfb6647024bad40afd51db7987ad3718cd
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-07-03 16:34:35 -0300

    Criando uma parada pra printar

--- app.ts
+++ app.ts
@@ -1,17 +1,22 @@
 let button = document.getElementById('button');
 
 let input1 = document.getElementById('input1' ) as HTMLInputElement
-let input2 = document.getElementById('button') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function Adicionar(number1: number, number2: number) {
-   return  number1 + number2
-}
+function Adicionar(number1: number, number2: number, devePrint:boolean, frase:string) {
+let resultado = number1 + number2;
+    console.log(frase + resultado)
 
+    return  number1 + number2
+   }
 
+let devePrint = true
+let frase : string
+frase = "o valor é "
 if(button){
     button.addEventListener('click', ()=>{
         if(input1 && input2){
-            Adicionar(Number(input1.value), Number(input2.value))
+            console.log(Adicionar(Number(input1.value), Number(input2.value), devePrint, frase))
         }
     })
 }
